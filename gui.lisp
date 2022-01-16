7e7c95ff67f0870556539c67aa88a5d659dcf308
Robert Strandh <robert.strandh@gmail.com>
2022-01-16 11:32:38 +0100

    Update display function to use a pixmap.

--- gui.lisp
+++ gui.lisp
@@ -3,12 +3,12 @@
 (clim:define-application-frame docleanser ()
   ((%image :initarg :image :reader image)
    (%transformation
-    :initform (clim:make-scaling-transformation* 0.4 0.4)
+    :initform (clim:make-scaling-transformation* 4/10 4/10)
     :accessor transformation))
   (:panes
    (image :application
           :scroll-bars nil
-          :height 1000
+          :height 2000
           :width 1500
           :display-function 'display-image)
    (inter :interactor :width 1500 :height 100))
@@ -19,8 +19,22 @@
 
 (defun display-image (frame pane)
   (let* ((transformation (transformation frame))
-         (pattern (clim:transform-region transformation (image frame))))
-    (clim:draw-pattern* pane pattern 0 0)))
+         (pattern (clim:transform-region transformation (image frame)))
+         (history (clim:stream-output-history pane)))
+    (multiple-value-bind (min-x min-y max-x max-y)
+        (clim:bounding-rectangle* pattern)
+      (declare (ignore min-x min-y))
+      (let ((pixmap (clim:with-output-to-pixmap
+                        (pixmap-medium pane
+                                       :width (ceiling max-x)
+                                       :height (ceiling max-y))
+                      (clim:draw-pattern* pixmap-medium pattern 0 0))))
+        (clim:add-output-record
+         (make-instance 'pixmap-output-record
+           :pixmap pixmap
+           :parent nil
+           :size nil)
+         history)))))
 
 (defun docleanser (filename)
   (clim:run-frame-top-level
